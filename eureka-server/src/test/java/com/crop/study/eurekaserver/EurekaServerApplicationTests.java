8c400f71a242cd093f0cc353555b43bb095be8e9
linmeng <1812323571@qq.com>
2021-10-18 11:25:22 +0800

    Initial commit

---
+++ eureka-server/src/test/java/com/crop/study/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crop.study.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
