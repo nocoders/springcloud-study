1109457b7a6924e570d8533fe4bab56bdd068206
linmeng <1812323571@qq.com>
2021-10-22 17:54:00 +0800

    zuul 过滤器添加

--- Finchley/eureka-server/src/main/java/com/crop/study/eurekaserver/EurekaServerApplication.java
+++ Finchley/eureka-server/src/main/java/com/crop/study/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * eureka 服务
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
